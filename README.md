7968f2664d6cb64ce7d84c7885ccc520d9edc6d5
bishax <alex.bishop@nesta.org.uk>
2021-08-23 13:48:50 +0100

    Add skeleton

---
+++ README.md
@@ -0,0 +1,7 @@
+# metaflow_custom (Nesta)
+
+Nesta's plugins for [Metaflow](metaflow.org).
+
+## Contributing
+
+TODO
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRES = ["metaflow>=2.3.4"]
+
+setup(
+    name="metaflow_custom",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    version="0.1.0",
+    description="Nesta plugins for Metaflow (metaflow.org)",
+    author="Nesta",
+    license="MIT",
+)
