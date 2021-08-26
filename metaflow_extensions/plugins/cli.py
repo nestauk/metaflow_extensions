cf78dcc4ec053e52a0118d0be49c9139c139a215
bishax <alex.bishop@nesta.org.uk>
2021-08-26 13:21:39 +0100

    Refactor CLI command s.t. preflow_kwargs can be over-ridden at CLI
    And help

--- metaflow_extensions/plugins/cli.py
+++ metaflow_extensions/plugins/cli.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from itertools import chain
 from os import PathLike
 from pathlib import Path
@@ -25,9 +26,46 @@ def cli_custom(ctx):
     pass
 
 
-@cli_custom.command(help="Custom command")
+@cli_custom.command(
+    help="""Run the workflow using YAML config.
+
+    CONFIG_PATH should be a YAML file containing keys `preflow_kwargs` and
+    flow_kwargs`. Each of these keys should contain a dictionary of
+    options followed by a list of flags. `preflow_kwargs` go before the `run`
+    command and `flow_kwargs` go after.
+
+    The following YAML
+
+    \b
+      preflow_kwargs:
+          - environment: conda
+            datastore: local
+          - [no-pylint, quiet]
+      flow_kwargs:
+          - tag: example
+          - with: batch
+          - [help]
+
+    becomes equivalent to
+
+    \b
+    $ python <flow script> \\
+       --environment conda \\
+       --datastore local \\
+       --no-pylint \\
+       --quiet \\
+       run \\
+       --tag example \\
+       --with batch \\
+       --help \\
+    """
+)
 @click.argument("config-path")
-@click.option("--key", default=None, help="Key in `config-path`")
+@click.option(
+    "--key",
+    default=None,
+    help="Key in CONFIG_PATH where preflow_kwargs and flow_kwargs are located",
+)
 @click.pass_context
 def run_config(ctx, config_path, key):
     """Plugin command: run-config.
@@ -42,25 +80,32 @@ def run_config(ctx, config_path, key):
         raise MetaflowConfigNotFound(config_path)
 
     config = _parse_config(config_path)[key] if key else _parse_config(config_path)
+    preflow_kwargs = _parse_options(config["preflow_kwargs"])
+    flow_kwargs = _parse_options(config["flow_kwargs"])
+    run_id = _add_run_id(config_path, key)
 
     # XXX: Hack to avoid double
     #      "Metaflow <version> executing <flow_name> for user:<user>"
     _erase_last_output_line()
 
+    # XXX: We over-ride sys.argv to add in our config and 'forward' to the run
+    #      command.
+    #      YAML defined preflow_kwargs have lower precedence so go first
+    flow_file = sys.argv[0]
+    cli_preflow_kwargs = sys.argv[1 : sys.argv.index("run-config")]
+    sys.argv = [
+        flow_file,
+        *preflow_kwargs,
+        *cli_preflow_kwargs,
+        "run",
+        *flow_kwargs,
+        *run_id,
+    ]
+
     # Import must be encapsulated to avoid partial imports
     from metaflow.cli import start
 
-    start.main(
-        [
-            *_parse_options(config["preflow_kwargs"]),
-            "run",
-            *_parse_options(config["flow_kwargs"]),
-            *_add_run_id(config_path, key),  # TODO: what about key
-            # TODO: add run-id-file
-        ],
-        obj=ctx.obj,
-        auto_envvar_prefix="METAFLOW",
-    )
+    start(obj=ctx.obj, auto_envvar_prefix="METAFLOW")
 
 
 def _add_run_id(config_path: Path, key=None) -> Tuple[str, str]:
