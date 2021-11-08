bc6a95862997d0d1a4ea48610960cddb2383a5e0
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-11-08 11:00:57 +0000

    fix secho args, add tests for coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import shutil
 from pathlib import Path
 
@@ -7,16 +8,27 @@ from metaflow_extensions.utils import local_pip_install
 from utils import remove_pkg  # noqa: I
 
 
-MYPROJECT_PATH = Path(__file__).parent / "myproject"
+@contextmanager
+def temporary_project_maker(tmpdir_factory, project_name):
+    project_path = Path(__file__).parent / project_name
+    temp = tmpdir_factory.mktemp("data-project")
+    shutil.copytree(project_path, temp / project_name)
+    yield temp
+    shutil.rmtree(temp)
 
 
 @pytest.fixture(scope="session")
 def temporary_project(tmpdir_factory):
     """Create temporary project `myproject`."""
-    temp = tmpdir_factory.mktemp("data-project")
-    shutil.copytree(MYPROJECT_PATH, temp / "myproject")
-    yield temp
-    shutil.rmtree(temp)
+    with temporary_project_maker(tmpdir_factory, "myproject") as temp:
+        yield temp
+
+
+@pytest.fixture(scope="session")
+def temporary_duffproject(tmpdir_factory):
+    """Create temporary project `myproject`."""
+    with temporary_project_maker(tmpdir_factory, "myduffproject") as temp:
+        yield temp
 
 
 @pytest.fixture
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -11,6 +11,17 @@ def test_runs_locally(temporary_installed_project):
     run_flow(flow_name(temporary_installed_project))
 
 
+def test_duff_project_runs_locally(temporary_duffproject):
+    """Flow should run but `myduffproject` won't have been installed since the project is duff.
+    The flow itself should test whether the error handling is working (fixes bug
+    https://github.com/nestauk/metaflow_extensions/issues/23)
+    """
+    with ch_dir(temporary_duffproject / "myduffproject"):
+        run_flow(
+            temporary_duffproject / "myduffproject/myduffproject/flows/duff_flow.py"
+        )
+
+
 def test_runs_conda(temporary_project):
     """`flow.py` should run due to `LocalProject` default environment."""
     with ch_dir(temporary_project / "myproject"):
